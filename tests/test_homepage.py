87808d6b2b1e6cab256e319c6f45dbdabd33d2a0
alexhvorostov <hvorostov.alex1@gmail.com>
2022-11-18 23:46:54 +0400

    import and use expected conditions in project

--- tests/test_homepage.py
+++ tests/test_homepage.py
@@ -1,8 +1,17 @@
 import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 @pytest.mark.usefixtures('setup')
 class TestHomepage:
 
-    def test_homepage(self):
-        pass
+    def test_auth_homepage(self):
+        driver = webdriver.Chrome
+        driver.implicitly_wait(10)
+
+        wait = WebDriverWait(driver, 10)
+        name_input_field = wait.until(ec.visibility_of_element_located((By.CLASS_NAME, 'mdc-text-field__input')))
+
